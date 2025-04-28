47cb4a7ce6cf7088659815e68054836c20d2184e
banzade <banzade98@gmail.com>
2025-04-28 23:29:34 +0530

    Creating basic init.lua using kickstarter.nvim

--- init.lua
+++ init.lua
@@ -1,15 +1,23 @@
 -- Set leader key
 vim.g.mapleader = " "
 
+-- Set to true if you have a Nerd font
+vim.g.have_nerd_font = true
+
 -- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Enable mouse mode, for example in resizing splits
+vim.opt.mouse = 'a'
 
 -- Tabs and identation
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+-- Save undo history
+vim.opt.breakindent = true
 
 -- Searching
 vim.opt.ignorecase = true
@@ -19,10 +27,10 @@ vim.opt.smartcase = true
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.cmd("colorscheme desert")
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.cursorline = true
 
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
-
--- Better splitting
-vim.opt.splitright = true
-vim.opt.splitbelow = true

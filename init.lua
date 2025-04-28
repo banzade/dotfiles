512f926e5e60536ed9f0de585cfbe71fe7f868b4
banzade <banzade98@gmail.com>
2025-04-29 01:08:12 +0530

    Using 'Henry-Misc YT' created skeleton for init.lua using lazy as package manager

--- init.lua
+++ init.lua
@@ -1,36 +1,20 @@
--- Set leader key
-vim.g.mapleader = " "
+-- imports
+require 'core.options'
+require 'core.keymaps'
 
--- Set to true if you have a Nerd font
-vim.g.have_nerd_font = true
+-- [[ Install `lazy.nvim` plugin manager ]]
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
 
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Enable mouse mode, for example in resizing splits
-vim.opt.mouse = 'a'
-
--- Tabs and identation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
--- Save undo history
-vim.opt.breakindent = true
-
--- Searching
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Appearance
-vim.opt.termguicolors = true
-vim.opt.background = "dark"
-vim.cmd("colorscheme desert")
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.cursorline = true
-
--- Clipboard
-vim.opt.clipboard = "unnamedplus"
+-- Set up plugins
+require('lazy').setup {
+  require('plugins.neotree'),
+  require('plugins.theme'),
+}

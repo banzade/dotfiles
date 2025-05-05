2b7535f4c5225824105e9bc7d35cab2989b00149
banzade <banzade98@gmail.com>
2025-05-05 22:57:33 +0530

    Used lazyvim: will customize later

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,2 @@
--- imports
-require 'core.options'
-require 'core.keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
-
--- Set up plugins
-require('lazy').setup {
-  require('plugins.neotree'),
-  require('plugins.theme'),
-}
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -117,3 +117,7 @@ alias ll="exa -lah --icons"
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 export PATH="$PATH:/opt/nvim/"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion

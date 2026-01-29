550a65affe5289db0dd1a835e0af217400782c8b
banzade <banzade98@gmail.com>
2026-01-29 19:27:08 +0530

    Added nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.opt.clipboard = "unnamedplus"
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true -- Great for jumping lines: e.g., '5j'
+
+-- Tab/Indent settings 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- UI niceties
+vim.opt.termguicolors = true  -- Makes colors look better in Alacritty
+vim.opt.cursorline = true     -- Highlights the line your cursor is on
+vim.opt.mouse = 'a'           -- Allows clicking/scrolling with mouse
